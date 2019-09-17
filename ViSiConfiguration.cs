a3913926afb3a9f1e8ce96f84e33d5f06016bf2f
Vadim Peretokin <vperetokin@gmail.com>
2019-09-17 11:10:47 +0200

    Add a ViSiconfiguration

---
+++ ViSiConfiguration.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using myfacade.Models;
+using Vonk.Core.Pluggability;
+using Vonk.Core.Repository;
+
+namespace myfacade
+{
+  [VonkConfiguration(order: 240)]
+  public static class ViSiConfiguration
+  {
+    public static IServiceCollection AddViSiServices(this IServiceCollection services, IConfiguration configuration)
+    {
+      services.AddDbContext<ViSiContext>();
+      services.TryAddSingleton<ResourceMapper>();
+      services.TryAddScoped<ISearchRepository, ViSiRepository>();
+
+      services.Configure<DbOptions>(configuration.GetSection(nameof(DbOptions)));
+      return services;
+    }
+  }
+}
