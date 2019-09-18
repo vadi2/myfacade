7affc9b566fec8d1292a783ea52faf127308f44f
Vadim Peretokin <vperetokin@gmail.com>
2019-09-18 11:16:26 +0200

    Fleshed out patient mapping

--- ResourceMapper.cs
+++ ResourceMapper.cs
@@ -3,23 +3,37 @@ using Hl7.Fhir.Model;
 using Hl7.Fhir.Support;
 using myfacade.Models;
 using Vonk.Fhir.R3;
+using System.Collections.Generic;
 
 namespace myfacade
 {
-    public class ResourceMapper
+  public class ResourceMapper
+  {
+    public IResource MapPatient(ViSiPatient source)
     {
-        public IResource MapPatient(ViSiPatient source)
-        {
-            var patient = new Patient
-            {
-                Id = source.Id.ToString(),
-                BirthDate = source.DateOfBirth.ToFhirDate()
-            };
-            patient.Identifier.Add(new Identifier("http://mycompany.org/patientnumber",
-                                                  source.PatientNumber));
-            // etc.
+      var patient = new Patient
+      {
+        Id = source.Id.ToString(),
+        BirthDate = source.DateOfBirth.ToFhirDate()
+      };
+      patient.Identifier.Add(new Identifier("http://mycompany.org/patientnumber",
+                                            source.PatientNumber));
 
-            return patient.ToIResource();
-        }
+      if (source.EmailAddress != null)
+      {
+        patient.Telecom.Add(new ContactPoint
+        {
+          System = ContactPoint.ContactPointSystem.Email,
+          Value = source.EmailAddress
+        });
+      }
+
+      var patientName = new HumanName { Use = HumanName.NameUse.Nickname };
+      patientName.Given = new List<string> { source.FirstName };
+      patientName.Family = source.FamilyName;
+      patient.Name.Add(patientName);
+
+      return patient.ToIResource();
     }
+  }
 }
